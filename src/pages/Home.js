2f4ca8ffbf3769c08ef902a4692190f14663e5ba
Letícia Damasio <damasioleticia03@gmail.com>
2022-04-01 15:50:59 -0300

    Deixando o site online

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,9 @@ import styled from 'styled-components'
 import CarouselMovies from './Carousel-Movies'
 import CarouselTV from './Carousel-Tv'
 
+const Container = styled.div`
+`
+
 export default class Home extends Component {
   render() {
     return (
