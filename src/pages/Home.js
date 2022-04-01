e106126958cf554e6d83fc6a0ee928d2b192e941
Letícia Damasio <damasioleticia03@gmail.com>
2022-04-01 16:11:57 -0300

    Tirei um styled que não estava utilizando

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
 import CarouselMovies from './Carousel-Movies'
 import CarouselTV from './Carousel-Tv'
-
-const Container = styled.div`
-`
 
 export default class Home extends Component {
   render() {
